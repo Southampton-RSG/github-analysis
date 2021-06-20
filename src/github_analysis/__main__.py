d525cd2237e6e34f62e1ae66645a72d90e0af67f
James Graham <j.graham@software.ac.uk>
2021-06-20 17:25:58 +0100

    feat: allow updating only a single data type

--- src/github_analysis/__main__.py
+++ src/github_analysis/__main__.py
@@ -19,11 +19,19 @@ def cli():
     logging.basicConfig(level=config('LOG_LEVEL', default='INFO'))
 
 
-def fetch_for_repos(repos: typing.Collection[str], fetchers: typing.Collection[fetch.FetcherFunc]) -> None:
+def fetch_for_repos(
+    repos: typing.Collection[str], fetcher_factory: fetch.Fetcher, only: typing.Optional[str] = None
+) -> None:
     """Apply each fetcher to each repo.
 
     Run a fetcher on all repos before moving on to the next fetcher.
     """
+    if only:
+        fetchers = [fetcher_factory.make(only)]
+
+    else:
+        fetchers = fetcher_factory.make_all()
+
     for fetcher in fetchers:
         for repo in repos:
             try:
@@ -36,28 +44,35 @@ def fetch_for_repos(repos: typing.Collection[str], fetchers: typing.Collection[f
 @cli.command()
 @click.option('-r', '--repo', 'repos', required=False, multiple=True)  # yapf: disable
 @click.option('-f', '--file', 'repo_file', required=False, type=click.File('r'))  # yapf: disable
-def fetch_all(repos: typing.Iterable[str], repo_file: typing.Optional[click.File]):
+@click.option('--only', required=False, type=click.Choice(fetch.GitHubFetcher.fetcher_paths.keys()))
+def fetch_all(
+    repos: typing.Iterable[str], repo_file: typing.Optional[click.File], only: typing.Optional[str] = None
+):
     if repo_file is not None:
         # Click has already opened the file for us
         repos = list(itertools.chain(repos, map(str.strip, repo_file)))
 
-    fetchers = fetch.GitHubFetcher().make_all()
-    fetch_for_repos(repos, fetchers)
+    fetcher_factory = fetch.GitHubFetcher()
+    fetch_for_repos(list(repos), fetcher_factory, only)
 
 
 @cli.command()
 @click.option('-r', '--repo', 'repos', required=False, multiple=True)  # yapf: disable
 @click.option('-f', '--file', 'repo_file', required=False, type=click.File('r'))  # yapf: disable
 @click.option('--import-root', required=True, type=click.Path(dir_okay=True, file_okay=False))
+@click.option('--only', required=False, type=click.Choice(fetch.FileFetcher.fetcher_paths.keys()))
 def import_existing(
-    repos: typing.Iterable[str], repo_file: typing.Optional[click.File], import_root: PathLike
+    repos: typing.Iterable[str],
+    repo_file: typing.Optional[click.File],
+    import_root: PathLike,
+    only: typing.Optional[str] = None
 ):
     if repo_file is not None:
         # Click has already opened the file for us
         repos = list(itertools.chain(repos, map(str.strip, repo_file)))
 
-    fetchers = fetch.FileFetcher(import_root).make_all()
-    fetch_for_repos(repos, fetchers)
+    fetcher_factory = fetch.FileFetcher(import_root)
+    fetch_for_repos(list(repos), fetcher_factory, only)
 
 
 if __name__ == '__main__':
